3dd852fb1bae9d0130ac98e659730a5c63b0a2b0
aliniko <aliverchan@gmail.com>
2022-01-05 23:16:08 +0100

    file and folder base structres

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  Header,
+  WhatAli,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import './App.css'; 
+
+
 const App = () => {
-  return <div> <h1> Ali Nekoh a Web Developer</h1></div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg" >
+          <Navbar />
+          <Header />
+      </div>
+      <Brand />
+      <WhatAli />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/containers/whatali/WhatAli.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatali.css';
+
+const WhatAli = () => {
+    return (
+        <div>
+            WhatAli
+        </div>
+    )
+}
+
+export default WhatAli
