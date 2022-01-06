ddc6acb7e4372a795c5f529bcc5edb9301766789
aliniko <aliverchan@gmail.com>
2022-01-06 23:43:07 +0100

    basic styles

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# portfolio website in React
 
+[color gradient: angrytools] (https://angrytools.com/gradient/) https://angrytools.com/gradient/
+[animation ](https://animista.net/play/basic)
--- src/App.js
+++ src/App.js
@@ -9,15 +9,14 @@ import {
   WhatAli,
 } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
-import './App.css'; 
-
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <div className="gradient__bg" >
-          <Navbar />
-          <Header />
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
       </div>
       <Brand />
       <WhatAli />
