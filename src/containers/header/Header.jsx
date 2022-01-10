6aa02cffefbd48fe3dc9ce9c7a373a1ad50df46a
aliniko <aliverchan@gmail.com>
2022-01-10 23:22:50 +0100

    header completed with small devices also

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import './header.css';
+import people from '../../assets/people.png';
+import ali from '../../assets/ali.svg';
+
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='nekoh__header section__padding id="home'>
+            <div className='nekoh__header-content'>
+                <h1 className='gradient__text'>I am a specialist on web developing </h1>
+                <p>skillful at programming and coding languages and other skills given bellow:</p>
+                <ul>
+                    <li>CSS/HTML/JavaScript/React, WordPress</li>
+                    <li>SEO, Responsive Design</li>
+                </ul>
+                <div className='nekoh__header-content__input'>
+                <input type="email" placeholder='Your Email' />
+                <button type="button"> Get Started</button>
+                </div>
+            <div className='nekoh__header-content__people'>
+                <img src={people} alt="" />
+                <p>1,600 people requested access a visit in last 24 hours</p>
+            </div>
+            </div>
+
+            <div className='nekoh__header-image'>
+                <img src={ali} alt="ali" />
+            </div>
+
         </div>
     )
 }
