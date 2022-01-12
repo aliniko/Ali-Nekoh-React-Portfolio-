cdcfea49fca0ebc94c7b459e3c71aa606b4a791c
aliniko <aliverchan@gmail.com>
2022-01-12 22:53:15 +0100

    features finished

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './feature.css';
 
-const Feature = () => {
-    return (
-        <div>
-            Feature
-        </div>
-    )
-}
+// const Feature = () => {
+//     return (
+//         <div className='nekoh__features-container__feature'>
+//             <div className='nekoh__features-container__feature-title'>
+//         </div>
+//         <h1>{title}</h1>
+//         </div>
+//         <div className='nekoh__features-container_feature-text'>
+//             <p>{text}</p>
+//         </div>
+//         </div>
+//     )
+// }
 
+const Feature = ({ title, text }) => (
+    <div className="nekoh__features-container__feature">
+      <div className="nekoh__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="nekoh__features-container_feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 export default Feature
--- src/containers/whatnekoh/WhatNekoh.jsx
+++ src/containers/whatnekoh/WhatNekoh.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Feature } from '../../components';
 import './whatnekoh.css';
+// import {Feature} from '../../components/feature/Feature';
+
 
 const WhatNekoh = () => {
     return (
         <div className='nekoh__whatnekoh section__margin' id='WhatNekoh'>
             <div className='nekoh__whatnekoh-feature'>
-                <Feature />
+                <Feature title="Who is Nekoh" text="We so opinion friends me message as delight." />
             </div>
             <div className='nekoh__whatnekoh-heading'>
                 <h1 className='gradient__text'>Grow your organization</h1>
                 <p>Explore Library</p>
             </div>
             <div className='nekoh__whatnekoh-container'>
-                <Feature />
-                <Feature />
-                <Feature />
+            <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+            <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+            <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
             </div>
         </div>
     )
