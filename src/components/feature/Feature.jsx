812245c78aa92dc0a5c360411f67d4a1782813c1
aliniko <aliverchan@gmail.com>
2022-01-13 22:58:23 +0100

    features tyles

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,19 +1,6 @@
 import React from 'react'
 import './feature.css';
 
-// const Feature = () => {
-//     return (
-//         <div className='nekoh__features-container__feature'>
-//             <div className='nekoh__features-container__feature-title'>
-//         </div>
-//         <h1>{title}</h1>
-//         </div>
-//         <div className='nekoh__features-container_feature-text'>
-//             <p>{text}</p>
-//         </div>
-//         </div>
-//     )
-// }
 
 const Feature = ({ title, text }) => (
     <div className="nekoh__features-container__feature">
@@ -21,7 +8,7 @@ const Feature = ({ title, text }) => (
         <div />
         <h1>{title}</h1>
       </div>
-      <div className="nekoh__features-container_feature-text">
+      <div className="nekoh__features-container__feature-text">
         <p>{text}</p>
       </div>
     </div>
