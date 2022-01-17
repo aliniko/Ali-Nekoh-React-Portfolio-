5b9aece244abfc8c1f32c6267fa077a1e80be0ab
aliniko <aliverchan@gmail.com>
2022-01-17 22:43:15 +0100

    possibility section

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 [color gradient: angrytools] (https://angrytools.com/gradient/) 
 [animation ](https://animista.net/play/basic)
 =======
-# currently working with Reactjs to rebuild my portfolio from scratch 
+### currently working with Reactjs to rebuild my portfolio from scratch 
 It is fun and amazing working with React 
 [svg logo / vector logo](vectr.com)
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import possibilityImage2 from '../../assets/possibilityImage2.jpg'
 import './possibility.css';
 
 const Possibility = () => {
     return (
-        <div>
-            Possibility
+        <div className='nekoh__possibility section__padding' id='possibility'>
+            <div className='nekoh__possibility-image'>
+                <img src={possibilityImage2} alt="possibility" />
+            </div>
+            <div className='nekoh__possibility-content'>
+                <h4>Request for Price</h4>
+                <h1 className='gradient__text'>WHY CHOOSE US Making it simple</h1>
+                <p>I focus to improve radically your product/services profile across all the online medium.</p>
+                <h4>Request To Get Started</h4>
+            </div>
         </div>
     )
 }
