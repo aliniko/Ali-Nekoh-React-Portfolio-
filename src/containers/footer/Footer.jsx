c57b8a45fc25b36cf3f493d8c6fce19df45a960d
aliniko <aliverchan@gmail.com>
2022-01-21 00:10:37 +0100

    footer styles completed

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -4,33 +4,33 @@ import nekoh from "../../assets/nekoh2.svg";
 
 const Footer = () => {
     return (
-        <div classNameName='nekoh__footer section__padding'>
-            <div classNameName='nekoh__footer-heading'>
-                <h1 classNameName='gradient__text'>Interested in collaborating or investing?</h1>
+        <div className='nekoh__footer section__padding'>
+            <div className='nekoh__footer-heading'>
+                <h1 className='gradient__text'>Interested in collaborating or investing?</h1>
             </div>
-            <div classNameName='nekoh__footer-btn'>
+            <div className='nekoh__footer-btn'>
                 <p>Start a conversation</p>
             </div>
 
-            <div classNameName='nekoh__footer-links'>
-                <div classNameName='nekoh__footer-links_logo'>
+            <div className='nekoh__footer-links'>
+                <div className='nekoh__footer-links_logo'>
                     <img src={nekoh} alt="Nekoh" />
                     <p>Frankfurter Str.126, Hofheim main taunus, Germany</p>
                 </div>
-                <div classNameName='nekoh__footer-links_div'>
+                <div className='nekoh__footer-links_div'>
                     <h4>Links</h4>
                     <p>TWITTER</p>
                     <p>GITHUB</p>
                     <p>LINKEDIN</p>
                 </div>
 
-                <div classNameName='nekoh__footer-links_div'>
+                <div className='nekoh__footer-links_div'>
                     <h4>Company</h4>
                     <p>Terms & Conditions</p>
                     <p>Privacy and Policy</p>
                     <p>Contact</p>
                 </div>
-                <div classNameName='nekoh__footer-links_div'>
+                <div className='nekoh__footer-links_div'>
                     <h4>Get in touch</h4>
                     <p>Hofheim, Germany</p>
                     <p>+49 15730415200</p>
